57437d8e94d49daa57c87b96760e936d700758bc
Kaue <mendoncakau@gmail.com>
2023-08-29 18:47:56 -0300

    commit

--- src/main/java/com/aula/sub/SubApplication.java
+++ src/main/java/com/aula/sub/SubApplication.java
@@ -3,8 +3,11 @@ package com.aula.sub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SubApplication {
 
     public static void main(String[] args) {
